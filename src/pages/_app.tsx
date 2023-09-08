8ceb05451e6ceb0da6967784f41524e3274944b8
joao5142 <joaopauloneto3687@gmail.com>
2023-09-08 10:39:18 -0300

    removing skelekton

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,6 @@ import { GlobalStyles } from "@/styles/globalStyle";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "@/styles/theme";
 
-import "react-loading-skeleton/dist/skeleton.css";
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={defaultTheme}>
